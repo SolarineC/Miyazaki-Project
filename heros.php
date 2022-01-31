2b29b7f6ab5b7a42e0f7fd33b46f19bcd7ee992f
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2022-01-31 22:14:24 +0100

    Page héros fonctionnelle

--- heros.php
+++ heros.php
@@ -1,6 +1,3 @@
-<?php include('templates/api_database.php'); ?>
-
-<!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
@@ -9,67 +6,63 @@
    <title>Miyazaki's People</title>
    <link rel="stylesheet" href="./style.css">
 </head>
-<body id="heros">>
-   
-   <section id="first-people">       
-         <div class="box1-people"></div>        
-         <h2><span>Mi</span>yazaki's <span>Pe</span>ople</h2>
-         <a href="index.php"><img class="arrow arrow-back" src="images/arrow-right.png" alt=""></a>
+
+<?php 
+require("./BDD/connectDB.php");
+
+if (isset($_GET["film"]) && !empty($_GET["film"])){
+   // On récupère les héros du film
+   $sql = "SELECT * FROM hero WHERE film = ".$_GET["film"];
+} else if (isset($_GET["page"]) && !empty($_GET["page"])) {
+   // On récupère les héros paginés
+   $limit = 5;
+   $page = ($_GET["page"] * $limit) - $limit;
+   $currentPage = $_GET["page"];
+   $sql = "SELECT COUNT(*) as nb_hero FROM hero";
+   $query = $connexion->prepare($sql);
+   $query->execute();
+   $nb_heros = $query->fetch(PDO::FETCH_ASSOC);
+   $nb_heros = $nb_heros["nb_hero"];
+   $nb_page = ceil($nb_heros / $limit);
+   $sql = "SELECT * FROM hero LIMIT ".$page.",".$limit;
+} else {
+   // On récupère tous les héros
+   $sql = "SELECT * FROM hero";
+}
+$query = $connexion->prepare($sql);
+$query->execute();
+$heros = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<body id="heros">
+   <section id="first-people" style="height: auto;">       
+      <div class="box1-people"></div>        
+      <h2><span>Mi</span>yazaki's <span>Pe</span>ople</h2>
+      <a href="index.php"><img class="arrow arrow-back" src="images/arrow-right.png" alt=""></a>
    </section>
-   
-   <section class="section-card">
-      <?php         
-         for ($i=0; $i < count($array_heros); $i++){
-            $nom_heros = $array_heros[$i]['nom'];
-            $film = $array_heros[$i]['film'];
-            $description = $array_heros[$i]['description'];
-            $roles = $array_heros[$i]['role'];
-            $image = $array_heros[$i]['trailer'];
-      ?>
-      <section id="second-people" class="slidershow">
-         <div class="slides">
-            <input type="radio" name="r" id="r1" checked>
-            <input type="radio" name="r" id="r2" checked>
-            <input type="radio" name="r" id="r3" checked>
-            <div class="slide s1">   
-               <div class="card-people">
-                  <div class="content-people">
-                     <h2><?php echo htmlspecialchars($nom_heros); ?></h2>
-                     <p>"<?php echo htmlspecialchars($description); ?>"</p>
-                     <p>Rôle: <?php echo htmlspecialchars($roles); ?></p>
-                     <a href="https://fr.wikipedia.org/wiki/Le_Ch%C3%A2teau_ambulant#Calcifer">Read More</a>
-                  </div>
-                  <img class="image-people" src="<?php echo htmlspecialchars($image); ?>" alt="">
-               </div>
-            </div> 
-            <div class="slide s2">
-               <div class="card-people">
-                  <div class="content-people">                            <h2>Jenkins Pendragon</h2>
-                     <p>"Possède le chateau ambulant".</p>
-                     <p>Rôle: Sorcier</p>
-                     <a href="https://fr.wikipedia.org/wiki/Le_Ch%C3%A2teau_ambulant#Hauru">Read More</a>
-                  </div>                
-                  <img class="image-people" src="/images/Jenkins Pendragon.png" alt="">
-               </div>
-            </div>
-            <div class="slide s3">
-               <div class="card-people">
-                  <div class="content-people">                            <h2>Sophie Hatter</h2>
-                     <p>"Innocente jeune fille qui se fait transformer par la sorcière"</p>
-                     <p>Rôle: Héroine</p>
-                     <a href="https://fr.wikipedia.org/wiki/Le_Ch%C3%A2teau_ambulant#Sophie">Read More</a>
-                  </div>
-                  <img class="image-people" src="/images/Sophie Hatter.png" alt="">
-               </div>
-            </div>
-         </div>
-         <div class="navigation">
-            <label for="r1" class="bar"></label>
-            <label for="r2" class="bar"></label>
-            <label for="r3" class="bar"></label>
+
+   <section>
+      <?php 
+      if (count($heros) == 0){
+         echo "<h2>Aucun Héro pour ce film !<h2>";
+      }
+      foreach ($heros as $hero) { ?>
+         <div>
+            <ul>
+               <li>Nom : <?= $hero["nom"] ?></li>
+               <li>Description : <?= $hero["description"] ?></li>
+               <li>Rôle : <?= $hero["role"] ?></li>
+            </ul>
          </div>
       <?php } ?>
-      </section>
+   </section>  
+
+   <section>
+      <?php 
+      for ($i=1; $i <= $nb_page; $i++){ ?>
+         <a href="./heros.php?page=<?= $i ?>"><?= $i ?></a>
+      <?php } ?>
    </section>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php include('templates/mainSection.php') ?>
-
-<!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
@@ -9,8 +6,22 @@
    <link rel="stylesheet" href="./style.css">
    <title>Miyazaki's World</title>
 </head>
-<body id="index">
 
+<?php 
+require("./BDD/connectDB.php"); 
+
+$sql = "SELECT * FROM film";
+$query = $connexion->prepare($sql);
+$query->execute();
+$films = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+
+
+
+
+<body id="index">
    <section id="first">
       <h2><span>Mi</span>yazaki's <span>Wo</span>rld</h2>         
       <div class="box1"></div>
@@ -19,6 +30,38 @@
          <span style="--i:1;"></span>
          <span style="--i:2;"></span>
          <span style="--i:3;"></span>
-         <a href="heros.php"><img class="arrow" src="images/arrow-left.png" alt=""></a>
+         <a href="heros.php?page=1"><img class="arrow" src="images/arrow-left.png" alt=""></a>
       </div>
    </section>
+   <section id="second">
+      <?php 
+      foreach ($films as $film) { 
+         $sql = "SELECT genre.nom FROM genre INNER JOIN genre_film ON genre_film.id_genre = genre.id INNER JOIN film ON genre_film.id_film = film.id WHERE film.id = :id_film";
+         $query = $connexion->prepare($sql);
+         $query->bindValue(":id_film", $film["id"]);
+         $query->execute();
+         $genres = $query->fetchAll(PDO::FETCH_ASSOC);        
+         ?>
+         <div class="card">
+            <div class="box2" id="b1" style="background: url(<?php echo htmlspecialchars($film["image"]); ?>" >
+               <div class="content">
+                  <a href=<?php echo htmlspecialchars($film["image"]); ?>><h3><?php echo htmlspecialchars($film["nom"]); ?></h3></a>
+                  <p>Genre:</br>
+                  <?php 
+                  foreach($genres as $genre) {
+                     echo $genre["nom"]."<br>";
+                  } 
+                  ?>
+                  </p>
+                  <p>Année: <?php echo htmlspecialchars($film["annee"]); ?></p>
+                  <p><?php echo htmlspecialchars($film["note"]); ?></p>
+                  <a href=<?php echo htmlspecialchars($film["trailer"]); ?>>Bande-annonce</a>
+                  <a href="heros.php?film=<?php echo $film["id"] ?>">Voir les Héros</a>
+               </div>
+            </div>
+         </div>
+      <?php } ?>
+   </section>
+
+</body>
+</html>
